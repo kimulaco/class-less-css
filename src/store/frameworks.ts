fdc824f55fd7e16d1f513bf0363d245394b4a316
kimulaco <kimulaco@gmail.com>
2022-09-09 07:10:42 +0900

    feat: Use cache framework stat

--- src/store/frameworks.ts
+++ src/store/frameworks.ts
@@ -1,15 +1,56 @@
+import Cookies from 'js-cookie'
 import { useEffect, useCallback } from 'react'
 import { atom, useRecoilState } from 'recoil'
 import { FRAMEWORKS } from '../constants/framework'
 import { FrameworkType } from '../types/framework'
 import { api } from '../utils/api'
 
+const getFrameworkStatCookieId = (id: FrameworkType['id']): string => {
+  return `clc_stat_${id}`
+}
+
+const parseCookieStat = (v: string): Required<FrameworkType>['stat'] => {
+  const json = JSON.parse(v || '{}')
+  if (typeof json.starCount !== 'number') {
+    throw new Error('The starCount type is invalid')
+  }
+  if (
+    typeof json.downloadCount !== 'number' &&
+    typeof json.downloadCount !== 'undefined'
+  ) {
+    throw new Error('The downloadCount type is invalid')
+  }
+  return {
+    starCount: json.starCount,
+    downloadCount: json.downloadCount,
+  }
+}
+
 export const addFrameworkStat = async (
   framework: FrameworkType,
 ): Promise<FrameworkType> => {
+  const cookieKey = getFrameworkStatCookieId(framework.id)
+  const cacheStat = Cookies.get(cookieKey)
+
+  try {
+    const data = parseCookieStat(cacheStat || '{}')
+    console.log(data)
+    return {
+      ...framework,
+      stat: {
+        starCount: data.starCount,
+        downloadCount: data.downloadCount,
+      },
+    }
+  } catch (error) {
+    // ignore
+  }
+
   try {
     const { data } = await api.get(`/api/stat/${framework.githubRepository}`)
+    console.log(data)
     if (data.stat) {
+      Cookies.set(cookieKey, JSON.stringify(data.stat), { expires: 1 })
       return {
         ...framework,
         stat: data.stat,
